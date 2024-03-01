55167146c3b15209ef663c7acb3448b375e241da
Isaac Ng <whn204@blue10.ex.ac.uk>
2024-03-01 14:08:02 +0000

    second commit

--- test_unit_Factorial.py
+++ test_unit_Factorial.py
@@ -11,6 +11,6 @@ def test_unit_calculate():
   assert result == expected
 
 @pytest.mark.parametrize("n,expected",
-  [ (1,1), (5,120), (7,5040), (9,362880) ])
+  [ (1,1), (5,120), (7,5040), (8,362880) ])
 def test_unit_calculate2(n,expected):
   assert Factorial.calculate(n) == expected
